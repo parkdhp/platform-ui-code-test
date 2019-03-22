af53f029fd4aeb3cb720b0ee969bf43948900e7a
Peter Park <parkdhp@gmail.com>
2019-03-22 06:39:41 -0400

    added ListComponent and ProviderCardComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+import { ProviderCardComponent } from './provider-card/provider-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    ProviderCardComponent
   ],
   imports: [
     BrowserModule
