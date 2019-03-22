b9adde48db24b00a52d411e4d4adcdf4c0248e69
Peter Park <parkdhp@gmail.com>
2019-03-22 06:41:32 -0400

    added methods to save to local storage, get local storage, swap provider, save provider, remove provider

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -29,8 +29,57 @@ export class ListComponent implements OnInit {
     }
   ];
 
+  // saves selected and unselected providers to local storage
+  saveToLocalStorage(): void {
+    localStorage.setItem('selectedProviders', JSON.stringify(this.selectedProviders));
+    localStorage.setItem('unselectedProviders', JSON.stringify(this.unselectedProviders));
+  }
+
+  // gets selected and unselected providers from local storage
+  getLocalStorage(): void {
+    this.selectedProviders = JSON.parse(localStorage.getItem('selectedProviders'));
+    this.unselectedProviders = JSON.parse(localStorage.getItem('unselectedProviders'));
+  }
+
+  // swaps provider from one array to another by id and saves to local storage
+  swapProvider(id, arr1, arr2): void {
+    for(let i = 0 ; i < arr1.length; i++) {
+      if(id === arr1[i].id) {
+        arr2.push(arr1[i])
+        arr1.splice(i,1);
+        this.saveToLocalStorage();
+        // prevent iteration since target provider reached
+        return;
+      }
+    }
+  }
+
+  // saves unselectedProvider to selectedProviders array 
+  saveProvider(e): void {
+    this.getLocalStorage();
+    this.swapProvider(e.target.id, this.unselectedProviders, this.selectedProviders);
+  }
+
+  // removes selected provider to unselectedProviders array
+  removeProvider(e): void {
+    this.getLocalStorage();
+    this.swapProvider(e.target.id, this.selectedProviders, this.unselectedProviders);
+  }
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // checks if providers have been saved to local storage
+    const initialized = JSON.parse(localStorage.getItem('initialized')) || false;
+
+    if(!initialized) {
+      // if not in local storage, save to local storage
+      localStorage.setItem('initialized', JSON.stringify(true));
+      this.saveToLocalStorage();
+    } else {
+      // get providers from local storage
+      this.getLocalStorage();
+    }
+  }
 
 }
